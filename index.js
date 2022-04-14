f1ff59af2fb6542d485165d68606a91bb1166d29
Mwai <machariamwai@gmail.com>
2022-04-14 21:34:03 +0300

    Pass all tests

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ document.querySelector("main#main").remove();
 
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
-newHeader.innerHTML = "Mwai is the champion!";
+newHeader.innerHTML = "Mwaii is the champion!";
