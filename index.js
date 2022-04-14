9765c086874b06b0373d755b26ae885185b167af
Mwai <machariamwai@gmail.com>
2022-04-14 21:32:39 +0300

    Pass all tests

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ document.querySelector("main#main").remove();
 
 const newHeader = document.createElement("h1");
 newHeader.id = "victory"
-newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.innerHTML = "Mwai is the champion!";
